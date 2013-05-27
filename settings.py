9f774857443d9b2165735b23fb1db4849d4f6232
Jeremy Diamond <jeremyscottdiamond@gmail.com>
2013-05-27 08:40:50 -0400

    redirect to inarticle.org (from the old corporaproject.com)

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/jsdiamond/corporaproject.com/corporasqlite3.db',    # Or path to database file if using sqlite3. #/corporaproject.com/Corpora/corporasqlite3.db
+        'NAME': '/home/jsdiamond/inarticle.org/corporasqlite3.db',    # Or path to database file if using sqlite3. #/corporaproject.com/Corpora/corporasqlite3.db
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -108,7 +108,7 @@ if not DEV_MODE:
         # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
         # Always use forward slashes, even on Windows.
         # Don't forget to use absolute paths, not relative paths.
-        '/home/jsdiamond/corporaproject.com/Corpora/templates',
+        '/home/jsdiamond/inarticle.org/Corpora/templates',
     )
 else:
     TEMPLATE_DIRS = (
